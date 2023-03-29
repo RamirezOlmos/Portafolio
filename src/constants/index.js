1a11bd96ef164bf8e8e85dac1dd3efdd5bc24064
RamirezOlmos <nyy_715074@yahoo.com>
2023-03-28 18:18:51 -0600

    added (src/constants/index.js) added a new project in the projects section

--- src/constants/index.js
+++ src/constants/index.js
@@ -20,6 +20,7 @@ import {
   imss,
   videogames,
   pulppo_landing,
+  openai_codex,
   threejs,
   santander,
   cindy_posadas,
@@ -245,27 +246,31 @@ const projects = [
     image: pulppo_landing,
     source_code_link: "https://github.com/RamirezOlmos",
   },
-  /* { */
-  /*   name: "Trip Guide", */
-  /*   description: */
-  /*     "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.", */
-  /*   tags: [ */
-  /*     { */
-  /*       name: "nextjs", */
-  /*       color: "blue-text-gradient", */
-  /*     }, */
-  /*     { */
-  /*       name: "supabase", */
-  /*       color: "green-text-gradient", */
-  /*     }, */
-  /*     { */
-  /*       name: "css", */
-  /*       color: "pink-text-gradient", */
-  /*     }, */
-  /*   ], */
-  /*   image: tripguide, */
-  /*   source_code_link: "https://github.com/", */
-  /* }, */
+  {
+    name: "OpenAI Codex",
+    description:
+      `Web-based platform has an elegant user interface that emulates the 
+       ChatGPT app. Seamlessly integrated with the advanced GPT-3 model API, 
+       users can submit their code to request translations to other programming 
+       languages, check for errors, or perform other programming tasks. Our system 
+       provides a user-friendly interface to streamline the programming experience.`,
+    tags: [
+      {
+        name: "javascript",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "OpenAI",
+        color: "green-text-gradient",
+      },
+      {
+        name: "nodejs",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: openai_codex,
+    source_code_link: "https://github.com/RamirezOlmos/openAI_codex_JS",
+  },
 ];
 
 export { services, technologies, experiences, testimonials, projects };
